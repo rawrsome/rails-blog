527076280b4359fa769d15e60fdcac640f53682e
rawrsome <christopher.tran@me.com>
2016-03-29 22:05:51 +0000

    update create, add flass and error message, add show by :id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,14 +4,24 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
-  
+  # create new article
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
     
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
+  
+  # show by :id
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   
   private 
     def article_params
